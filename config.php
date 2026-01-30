116190c268af0a5839f3ad7367e1ceab64677ad9
ego <ego@nomail.lan>
2026-01-30 17:00:28 +0100

    mod ai config modelurl

--- config.php
+++ config.php
@@ -3,10 +3,11 @@
 //declare(strict_types=1);
 
 /**
- * @env AI_PROVIDER_HUGGINGFACE=""
- * @env AI_PROVIDER_OPENAI=""
- * @env AI_PROVIDER_XAI=""
- * @env AI_PROVIDER_CLD=""
+ * @env AI_APIKEY_HUGGINGFACE=""
+ * @env AI_APIKEY_OPENAI=""
+ * @env AI_APIKEY_XAI=""
+ * @env AI_APIKEY_CLD=""
+ * @env AI_MODELURL_CLD=""
  * @env AI_DATADIR=".cryodrift/data/ai/"
  */
 
@@ -25,16 +26,16 @@ $cfg[\cryodrift\ai\Cli::class] = [
   'datadir' => Core::env('AI_DATADIR'),
 ];
 $modelproviders = [
-  new Provider('oairesp', 'https://api.openai.com/v1/responses', Core::env('AI_PROVIDER_OPENAI'), modelsurl: 'https://api.openai.com/v1/models', payload: 'hug'),
-  new Provider('xairesp', 'https://api.x.ai/v1/responses', Core::env('AI_PROVIDER_XAI'), modelsurl: 'https://api.x.ai/v1/models', payload: 'oai'),
-  new Provider('locresp', 'http://127.0.0.1:2001/v1/chat/completions', Core::env('AI_PROVIDER_CLD'), modelsurl: 'http://127.0.0.1:2001/v1/models', payload: 'hug'),
+  new Provider('oairesp', 'https://api.openai.com/v1/responses', Core::env('AI_APIKEY_OPENAI'), modelsurl: 'https://api.openai.com/v1/models', payload: 'hug'),
+  new Provider('xairesp', 'https://api.x.ai/v1/responses', Core::env('AI_APIKEY_XAI'), modelsurl: 'https://api.x.ai/v1/models', payload: 'oai'),
+  new Provider('locresp', 'http://127.0.0.1:2001/v1/chat/completions', Core::env('AI_APIKEY_CLD'), modelsurl: 'http://127.0.0.1:2001/v1/models', payload: 'hug'),
 ];
 $cfg[ParamProvider::class] = [
   'providers' => [
-    new Provider('hugchat', 'https://router.huggingface.co/v1/chat/completions', Core::env('AI_PROVIDER_HUGGINGFACE'), payload: 'hug'),
-    new Provider('oaichat', 'https://api.openai.com/v1/chat/completions', Core::env('AI_PROVIDER_OPENAI'), payload: 'hug'),
-    new Provider('xaichat', 'https://api.x.ai/v1/chat/completions', Core::env('AI_PROVIDER_XAI'), payload: 'hug'),
-    new Provider('cldresp', 'https://api.anthropic.com/v1/messages', Core::env('AI_PROVIDER_CLD'), ['x-api-key' => Core::env('AI_PROVIDER_CLD'), 'anthropic-version' => '2023-06-01'], payload: 'hug'),
+    new Provider('hugchat', 'https://router.huggingface.co/v1/chat/completions', Core::env('AI_APIKEY_HUGGINGFACE'), payload: 'hug'),
+    new Provider('oaichat', 'https://api.openai.com/v1/chat/completions', Core::env('AI_APIKEY_OPENAI'), payload: 'hug'),
+    new Provider('xaichat', 'https://api.x.ai/v1/chat/completions', Core::env('AI_APIKEY_XAI'), payload: 'hug'),
+    new Provider('cldresp', 'https://api.anthropic.com/v1/messages', Core::env('AI_APIKEY_CLD'), ['x-api-key' => Core::env('AI_APIKEY_CLD'), 'anthropic-version' => '2023-06-01'], modelsurl: Core::env('AI_MODELURL_CLD'), payload: 'hug'),
     ...$modelproviders
   ]
 ];
